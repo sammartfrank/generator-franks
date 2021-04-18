fdbd5f72d56f8d1f108aa84f0e578503673de305
sammartfrank <franksammartino7@gmail.com>
2021-04-18 14:04:00 -0300

    mod2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,31 @@
 module.exports = {
-    "root": true,
-    "extends": [
-      "standard",
-      "prettier",
-      "eslint:recommended"
+  root: true,
+  extends: ["standard", "prettier", "eslint:recommended"],
+  plugins: ["prettier", "plugin:react/recommended"],
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaVersion: 2016,
+    sourceType: "module",
+  },
+  env: {
+    browser: true,
+    commonjs: true,
+    node: true,
+    es6: true,
+    mocha: true,
+  },
+  rules: {
+    "no-debugger": "off",
+    "no-console": "off",
+    "prettier/prettier": [
+      "error",
+      {
+        semi: true,
+        bracketSpacing: true,
+        trailingComma: "es5",
+        printWidth: 120,
+        endOfLine: "auto",
+      },
     ],
-    "plugins": [
-      "prettier"
-    ],
-    "parser": "babel-eslint",
-    "parserOptions": {
-      "ecmaVersion": 2016,
-      "sourceType": "module"
-    },
-    "env": {
-      "browser": true,
-      "commonjs": true,
-      "node": true,
-      "es6": true,
-      "mocha": true
-    },
-    "rules": {
-      "no-debugger": "off",
-      "no-console": "off",
-      "prettier/prettier": [
-        "error",
-        {
-          "semi": true,
-          "bracketSpacing": false,
-          "trailingComma": "es5",
-          "printWidth": 120
-        }
-      ]
-    }
-  }
+  },
+};
---
+++ generators/react/app/simple/index.js
@@ -0,0 +1,8 @@
+  
+const Generator = require("yeoman-generator");
+
+module.exports = class extends Generator {
+  app() {
+    this.fs.copy(this.templatePath("app"), this.destinationPath("./src/app"));
+  }
+};
